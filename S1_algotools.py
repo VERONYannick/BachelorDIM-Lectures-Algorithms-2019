8f22ab52d70bf18ce9490b106c26d6730d3089af
Alexis3741 <alexisredcube@gmail.com>
2019-09-26 17:03:47 +0200

    maximum

--- S1_algotools.py
+++ S1_algotools.py
@@ -12,15 +12,23 @@ What can you expect if all the values are below zero ?
     =>ZeroDivisionError: division by zero
 '''
 import numpy as np
+import time
+import statistics as st
 
-def average_above_zero(arr):
+def average_above_zero(tab):
     """Function that calculate average of array of positives int
     Args:
         array: an array
     Returns the moy
     """
+    if not(isinstance(tab,list)):
+        raise ValueError('average_above_zero, expected a list as input')
+    if len(tab)==0:
+        raise ValueError('expected a non empty a list as input')
+    if not(isinstance(tab[0],(int,float))):
+        raise ValueError('average_above_zero, expected a list of numbers')
     #tab_zeros=np.zeros(12,dtype=np.int32)
-    tab_fromList=np.array(arr)
+    tab_fromList=np.array(tab)
     
     Som = 0
     N = 0
@@ -31,6 +39,21 @@ def average_above_zero(arr):
     Moy=Som/N
     return Moy
 
-tab_list=np.random.randint(0,100000,1000)#Random array of 1000 positives int
+def max_value(tab):
+    """Function that calculate max of array of int
+    Args:
+        array: an array
+    Returns the max
+    """
+    tab_fromList=np.array(tab)
+    max_var_index=0
+    for i,var in enumerate(tab_fromList):
+        if var>tab_fromList[max_var_index]:
+            max_var_index=i
+    return tab_fromList[max_var_index],max_var_index
+
+tab_list=np.random.randint(0,100000,1000).tolist()#Random array of 1000 positives int
 #tab_list=[10,15,24,16,85]
-print("moy =",average_above_zero(tab_list))
+average = average_above_zero(tab_list)
+print("moy =",average)
+print("max = ",max_value(tab_list))
